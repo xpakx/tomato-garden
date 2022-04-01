2f076018aa8d2b72fb4ad7aca3ccfe7b32ffd4f8
xpakx <55586470+xpakx@users.noreply.github.com>
2022-04-01 14:04:37 +0200

    feat(frontend): add option to restrict garden stats to specific dates

--- frontend/src/app/component/garden-modal/garden-modal.component.ts
+++ frontend/src/app/component/garden-modal/garden-modal.component.ts
@@ -1,5 +1,6 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { StatsResponse } from 'src/app/entity/stats-response';
 import { StatsService } from 'src/app/service/stats.service';
 
@@ -12,11 +13,34 @@ export class GardenModalComponent implements OnInit {
   @Output() closeEvent = new EventEmitter<boolean>();
   failed: number = 0;
   succeed: number = 0;
+  form: FormGroup;
 
-  constructor(private service: StatsService) { }
+  constructor(private service: StatsService, private fb: FormBuilder) { 
+    this.form = this.fb.group(
+      {
+        before: ['', []],
+        after: ['', []]
+      }
+    );
+  }
 
   ngOnInit(): void {
     this.service.getStats({tagId: undefined, after: undefined, before: undefined}).subscribe(
+      (response: StatsResponse) => {
+        this.failed = response.failed;
+        this.succeed = response.succeed;
+      },
+      (error: HttpErrorResponse) => {}
+    )
+  }
+
+  apply(): void {
+    let afterDate = new Date(this.form.controls.after.value);
+    let beforeDate = new Date(this.form.controls.before.value);
+    this.service.getStats({
+      tagId: undefined, 
+      after: afterDate, 
+      before: beforeDate}).subscribe(
       (response: StatsResponse) => {
         this.failed = response.failed;
         this.succeed = response.succeed;
