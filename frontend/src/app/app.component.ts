f7c2a1eba7c1f20ea8f39f486f2ee5d1d188f12e
xpakx <55586470+xpakx@users.noreply.github.com>
2022-04-08 12:55:55 +0200

    refactor(frontend/ui): add angular animations to menu

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,35 @@
+import { animate, style, transition, trigger, group, query } from '@angular/animations';
 import { Component } from '@angular/core';
 import { SettingsService } from './service/settings.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('slideMenuAnimation', [
+      transition(':enter', [
+        style({ width: '0px', overflow: 'hidden' }),
+        group([
+          animate('.15s cubic-bezier(.4,0,1,1)', style({ width: '!' })),
+          query('.menu-content',[
+            style({ transform: 'translateX(-150%)', width: '250px' }),
+            animate('.15s cubic-bezier(.4,0,1,1)', style({ transform: 'translateX(0)', width: '250px' }))
+          ])
+        ])
+      ]),
+      transition(':leave', [
+        style({ overflow: 'hidden' }),
+        group([
+          animate('.15s cubic-bezier(.4,0,1,1)', style({ width: '0' })),
+          query('.menu-content',[
+            style({ transform: 'translateX(0)' }),
+            animate('.15s cubic-bezier(.4,0,1,1)', style({ transform: 'translateX(-150%)' }))
+          ])
+        ])
+      ]),
+    ])
+  ]
 })
 export class AppComponent {
   title = 'tomato-garden';
